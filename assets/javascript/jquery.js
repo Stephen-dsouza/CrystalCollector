9af14019e3574b37745e9da3f1354656dec5d37a
Stephen D <stephen.roy.dsouza@gmail.com>
2019-07-11 18:13:18 +1000

    get the clicks working

--- assets/javascript/jquery.js
+++ assets/javascript/jquery.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-var score= 0;
+      var score = 0;
+      var loss = 0;
+
       //get random number
 
       var coguess = Math.floor(Math.random() * 100 + 20)
@@ -12,21 +14,37 @@ var score= 0;
 
 
       //output computer random number
-      
-      $("#randomv").append(coguess);
 
+      $("#randomv").append(coguess);
+      // $("#wins").text("SCORE IS",score);
       //assign values to the 4 crystals
       var crys1 = crystall();
       var crys2 = crystall();
       var crys3 = crystall();
+      var crys4 = crystall();
       // console.log(coguess);
       // console.log(crys1);
       // console.log(crys2);
       // console.log(crys3);
- // when each crystal is clicked,add the count andcheck if this value is = or > than computerguess
-      $("#c1").click(function(){
-score=crys1+score;
-console.log(score);
+      // when each crystal is clicked,add the count andcheck if this value is = or > than computerguess
+      $("#c1").click(function () {
+            score = crys1 + score;
+            console.log(score);
       });
+      $("#c2").click(function () {
+            score = crys2 + score;
+            console.log(score);
+
+      });
+      $("#c3").click(function () {
+            score = crys3 + score;
+            console.log(score);
+      });
+      $("#c4").click(function () {
+            score = crys4 + score;
+            console.log(score);
+      });
+
+
 
 });
