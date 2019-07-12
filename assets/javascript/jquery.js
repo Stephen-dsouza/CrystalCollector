f964dbf1805d7d5434bb01a446493c55a303aa62
Stephen D <stephen.roy.dsouza@gmail.com>
2019-07-12 21:31:22 +1000

    getting button to stop game after 10 losses

--- assets/javascript/jquery.js
+++ assets/javascript/jquery.js
@@ -1,79 +1,66 @@
 $(document).ready(function () {
 
-      var score = 0;
-      var loss = 0;
+                  var score = 0;
+                  var clickcount = 0;
+                  var loss = 0;
 
-      //get random number
+                  //get random number
 
-      var coguess = Math.floor(Math.random() * 100 + 20)
-      //get random number for 4 crystals
+                  function compguessed() {
+                        return Math.floor(Math.random() * 100 + 20)
+                  };
+                  //get random number for 4 crystals
 
-      function crystall() {
-            return Math.floor(Math.random() * 20 + 5)
-      }
-
-
-      //output computer random number
-
-      $("#randomv").append(coguess);
-      //output the wins
-      $("#wins").text("WINS: " + score);
-      console.log(score);
-      $("#loss").text("LOSSES: " + loss);
-      // $("#wins").text("SCORE IS",score);
-      //assign values to the 4 crystals
-      var crys1 = crystall();
-      var crys2 = crystall();
-      var crys3 = crystall();
-      var crys4 = crystall();
-
-      // console.log(coguess);
-      // console.log(crys1);
-      // console.log(crys2);
-      // console.log(crys3);
-      // when each crystal is clicked,add the count andcheck if this value is = or > than computerguess
-
-      $("#c1,#c2,#c3,#c4").click(function () {
-                  var id = this.id;
-                  if (score > coguess) {
-                        loss++;
-
-                  } 
-                  else if (score < coguess) {
-
-                        if (id == "c1") {
-                              score = crys1 + score;
-                              console.log(score);
-                              $("#wins").text("WINS: " + score);
-                        } else if (id == "c2") {
-                              score = crys2 + score;
-                              console.log(score);
-                        } else if (id == "c3") {
-                              score = crys3 + score;
-                              console.log(score);
-                        }
-                        else {
-                              score = crys4 + score;
-                              console.log(score);
-                        }
+                  function crystall() {
+                        return Math.floor(Math.random() * 20 + 5)
                   }
-                  else{}
-                  
 
-                  // $("#c2").click(function () {
-                  //       score = crys2 + score;
-                  //       console.log(score);
 
-                  // });
-                  // $("#c3").click(function () {
-                  //       score = crys3 + score;
-                  //       console.log(score);
-                  // });
-                  // $("#c4").click(function () {
-                  //       score = crys4 + score;
-                  //       console.log(score);
-                  // });
+                  //output computer random number
+                  coguess = compguessed();
+                  $("#randomv").text("YOUR TARGET IS :" + coguess);
 
-            
-      });
-});
+                  //assign values to the 4 crystals
+                  var crys1 = crystall();
+                  var crys2 = crystall();
+                  var crys3 = crystall();
+                  var crys4 = crystall();
+
+
+                  // when each crystal is clicked,add the count andcheck if this value is = or > than computerguess
+
+                  $("#c1,#c2,#c3,#c4").click(function () {
+                              var id = this.id;
+
+                              if (id == "c1" && clickcount < coguess) {
+                                    clickcount = crys1 + clickcount;
+                                    console.log(score);
+                                    console.log(clickcount);
+                                    $("#currentcount").text("YOUR CURRENT CLICK COUNT IS: " + clickcount);
+                              } else if (id == "c2" && clickcount < coguess) {
+                                    clickcount = crys2 + clickcount;
+                                    $("#currentcount").text("YOUR CURRENT CLICK COUNT IS: " + clickcount);
+                              } else if (id == "c3" && clickcount < coguess) {
+                                    clickcount = crys3 + clickcount;
+                                    $("#currentcount").text("YOUR CURRENT CLICK COUNT IS: " + clickcount);;
+                              } else if (id == "c4" && clickcount < coguess) {
+                                    clickcount = crys4 + clickcount;
+                                    $("#currentcount").text("YOUR CURRENT CLICK COUNT IS: " + clickcount);
+                              } else if (clickcount > coguess && loss<10) {
+                                    loss++;
+                                    $("#loss").text("LOSSES: " + loss);
+                                    coguess = compguessed();
+                                    $("#randomv").text("YOUR TARGET IS :" + coguess);
+                                    clickcount = 0;
+                              }
+                              else if (clickcount > coguess && loss >=10) {
+                                    $('.cancelButton').append('<input type="button" id="cancelRoomBtn" class="btn btn-danger" value="GAmeOver" />');
+                              }
+
+                        
+                              else {$("#win").text("WINS: " + loss);
+                              
+                        }
+                              
+                        });
+                  });
